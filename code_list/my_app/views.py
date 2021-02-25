b70287267ea491dd69a24dd2f830263b9398d5cf
sergiughimpExpleo <78013000+sergiughimpExpleo@users.noreply.github.com>
2021-02-25 14:38:15 +0200

    Create search - URL no working"

--- code_list/my_app/views.py
+++ code_list/my_app/views.py
@@ -1,6 +1,13 @@
+import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
+
 # Create your views here.
+
+BASE_CRAIGSLIST_URL = 'https://www.qwant.com/?q={}'
+# BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
 def home(request):
@@ -9,5 +16,14 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
+    models.Search.objects.create(search=search)
+    # print(quote_plus(search))
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = respose.text
+    soup = BeautifulSoup(data, features='html.parser')
+    post_titles = soup.find_all('a', {'class': 'result-title'})
+    print(post_titles[0].get('href'))
+    # print(data)
     stuff_for_frontend = {'search': search, }
     return render(request, 'my_app/new_search.html', stuff_for_frontend)
